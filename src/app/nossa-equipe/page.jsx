8cad80ae712daa50243b3dc8332f0708c7e8d03a
Isaias Gonçalves <131933508+IsaiasZaza@users.noreply.github.com>
2024-08-28 15:52:06 -0300

    atualizacoes

--- src/app/nossa-equipe/page.jsx
+++ src/app/nossa-equipe/page.jsx
@@ -49,7 +49,7 @@ export default function NossaEquipe() {
 
     return (
         <section className="py-12 bg-white">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-24">
                 <div className="text-center mb-8">
                     <h2 className="text-3xl font-extrabold text-gray-900">Nossa Equipe</h2>
                     <p className="text-lg text-gray-600">Conheça os profissionais que fazem a diferença.</p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,17 +8,16 @@ const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="bg-white shadow">
+    <header className="bg-white shadow fixed mx-auto w-full z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center py-4">
         <div className="flex items-center">
           <Image
-            src="/kokimoto.jpg" // Substitua com o caminho do seu logo
+            src="/logo.png" // Substitua com o caminho do seu logo
             alt="Logo Vaga Lume"
-            width={50}
-            height={50}
+            width={150}
+            height={100}
             className="mr-3"
           />
-          <a href="/">inicial</a>
         </div>
         <div className="hidden md:flex space-x-4">
           <nav>
@@ -32,7 +31,7 @@ const Header = () => {
           </nav>
         </div>
         <div className="hidden md:block">
-          <a href="#" className="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-600">Doe Agora</a>
+          <a href="#" className="bg-yellow-500 text-white px-4 py-16 rounded hover:bg-yellow-600">Doe Agora</a>
         </div>
         <div className="md:hidden flex items-center">
           <button onClick={() => setIsOpen(!isOpen)} className="text-gray-800 hover:text-yellow-500">
@@ -44,10 +43,11 @@ const Header = () => {
         <div className="md:hidden bg-white text-gray-800">
           <nav>
             <ul className="space-y-2 px-4 py-2">
-              <li><a href="#" className="block px-4 py-2 hover:text-yellow-500">Como Doar</a></li>
-              <li><a href="#" className="block px-4 py-2 hover:text-yellow-500">Quem Somos</a></li>
-              <li><a href="#" className="block px-4 py-2 hover:text-yellow-500">Nossa História</a></li>
-              <li><a href="#" className="block px-4 py-2 hover:text-yellow-500">O Que Fazemos</a></li>
+              <li><a href="#" className="block px-4 py-2 text-gray-800 hover:text-yellow-500">Como Doar</a></li>
+              <li><a href="#" className="block px-4 py-2 text-gray-800 hover:text-yellow-500">Quem Somos</a></li>
+              <li><a href="#" className="block px-4 py-2 text-gray-800 hover:text-yellow-500">Nossa História</a></li>
+              <li><a href="#" className="block px-4 py-2 text-gray-800 hover:text-yellow-500">O Que Fazemos</a></li>
+              <li><a href="/nossa-equipe" className="block px-4 py-2 text-gray-800 hover:text-yellow-500">Nossa Equipe</a></li>
               <li><a href="#" className="block px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600">Doe Agora</a></li>
             </ul>
           </nav>
