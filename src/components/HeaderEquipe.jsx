bcdb83245cc67d5fe05261e61feb464dfeefc6d6
GustaDev7 <tapofo.mendes@gmail.com>
2024-10-11 10:39:45 -0300

    feat: nossa equipe

---
+++ src/components/HeaderEquipe.jsx
@@ -0,0 +1,12 @@
+const HeaderEquipe = () => {
+    return (
+        <div className="bg-white text-center py-4 w-4/6 mx-auto ">
+            <p className="text-orange-500 font-medium text-2xl">
+            Por trás de cada transformação, há uma equipe apaixonada e dedicada. Conheça as pessoas que tornam possível o impacto positivo da Gamboa Ação.
+            </p>
+      </div>
+    );
+  };
+  
+  export default HeaderEquipe;
+  
